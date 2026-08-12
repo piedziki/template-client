7b0aec1d765683ba227d475052ff89c252ab9920
piedziki <maria_hs_souza@estudante.sesisenai.org.br>
2026-08-12 20:40:03 -0300

    Update popUp.js

--- src/components/popUp.js
+++ src/components/popUp.js
@@ -19,7 +19,7 @@ function PopUp({showPopUp, closePopUp}){
 
   const postLogin = async () => {
   try{
-    const response =  await axios.post('http://localhost:5000/login',{ 
+    const response =  await axios.post('https://template-server-5gb9.onrender.com/login',{ 
       email:email,
       senha:senha,
     },{
